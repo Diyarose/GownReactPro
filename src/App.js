e812d7fde8d5a6b0378348534744dcf9d2f593ee
Diyarose <dhiyarosedavis@gmail.com>
2024-06-13 01:59:49 +0530

    view

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AddGown from './components/AddGown';
 import SearchGown from './components/SearchGown';
+import ViewGown from './components/ViewGown';
 
 function App() {
   return (
     <div>
       <AddGown/>
       <SearchGown/>
+      <ViewGown/>
     </div>
   );
 }
